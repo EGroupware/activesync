bb68787dd2c48d74890e81661a7bd542edc65712
Ralf Becker <ralfbecker@outdoor-training.de>
2012-07-20 07:35:49 +0000

    switching E_STRICT off as in rest of EGroupware, as not all E_STRICT warning can be easyly fixed and it makes logs unusable

--- debug.php
+++ debug.php
@@ -48,7 +48,11 @@ function zarafa_error_handler($errno, $errstr, $errfile, $errline, $errcontext) 
     debugLog("$errfile:$errline $errstr ($errno)");
 }
 
-error_reporting(E_ALL & ~E_NOTICE);
-set_error_handler("zarafa_error_handler",E_ALL & ~E_NOTICE);
+// E_STRICT in PHP 5.4 gives various strict warnings in working code, which can NOT be easy fixed in all use-cases :-(
+// Only variables should be assigned by reference, eg. soetemplate::tree_walk()
+// Declaration of <extended method> should be compatible with <parent method>, varios places where method parameters change
+// --> switching it off for now, as it makes error-log unusable
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
+set_error_handler("zarafa_error_handler",E_ALL & ~E_NOTICE & ~E_STRICT);
 
 ?>

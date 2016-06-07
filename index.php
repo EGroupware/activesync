74230cb67c1086d52f202c84c294bc65db52aaa1
Ralf Becker <RalfBecker@outdoor-training.de>
2016-06-07 12:52:27 +0200

    updated config.php to zpush 2.3 one keeping our changes, including our header.inc.php in index.php (was in config.php), as zpush autoloaded need to be loaded last to find there "Log" class instead of PEAR one in our vendor

--- index.php
+++ index.php
@@ -11,8 +11,30 @@
  * @version $Id$
  */
 
+// disable globally enabled compression, as this causes issues with some devices
+// z-push handles output compression
+ini_set("zlib.output_compression",0);
+
+if (!isset($GLOBALS['egw_info']))
+{
+	$GLOBALS['egw_info'] = array(
+		'flags' => array(
+			'disable_Template_class'  => true,
+			'noheader'                => true,
+			'currentapp'              => 'login',
+			'no_exception_handler' => 'errorlog',	// only logs exceptions
+	));
+
+	require(__DIR__.'/../header.inc.php');
+}
+
+// EGroupware specific ZPush version from our GitHub clone, overwriting version.php from stock ZPush
+define('ZPUSH_VERSION', 'EGroupware-'.EGroupware\Api\Framework::api_version()/*.
+	(!file_exists(__DIR__.'/../vendor/z-push/z-push/.svn/entries') ? '' :
+		' (r'.trim(file_get_contents(__DIR__.'/../vendor/z-push/z-push/.svn/entries')).')')*/);
+
 // following code is necessary to use an unchanged z-push2 package installed in vendor/z-push/z-push
 $_SERVER['SCRIPT_FILENAME'] = __DIR__.'/vendor/z-push/z-push/src/index.php';
 chdir(__DIR__.'/vendor/z-push/z-push/src');
 define('ZPUSH_CONFIG', __DIR__.'/inc/config.php');
-include('vendor/z-push/z-push/src/index.php');
+include(__DIR__.'/vendor/z-push/z-push/src/index.php');

050ae8fe764d7bc241e9f7f8ee462ccd6a3a7368
Ralf Becker <RalfBecker@outdoor-training.de>
2016-06-09 20:36:12 +0200

    using SqlStateMaschine

--- setup/setup.inc.php
+++ setup/setup.inc.php
@@ -12,7 +12,7 @@
 /* Basic information about this app */
 $setup_info['activesync']['name']      = 'activesync';
 $setup_info['activesync']['title']     = 'eSync';
-$setup_info['activesync']['version']   = '16.1';
+$setup_info['activesync']['version']   = '16.1.001';
 $setup_info['activesync']['enable']    = 2;
 $setup_info['activesync']['app_order'] = 99;
 
@@ -41,3 +41,4 @@ $setup_info['activesync']['depends'][] = array(
 	 'appname'  => 'api',
 	 'versions' => Array('16.1')
 );
+	$setup_info['activesync']['tables'] = array('egw_zpush_states','egw_zpush_users','egw_zpush_settings');

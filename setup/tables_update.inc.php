43e7d881b49a2b06f80c4ba17e5b3227d838e561
Ralf Becker <RalfBecker@outdoor-training.de>
2020-06-28 10:12:54 +0200

    bump version to 20.1

--- setup/tables_update.inc.php
+++ setup/tables_update.inc.php
@@ -100,3 +100,13 @@ function activesync_upgrade17_1()
 {
 	return $GLOBALS['setup_info']['activesync']['currentver'] = '19.1';
 }
+
+/**
+ * Bump version to 20.1
+ *
+ * @return string
+ */
+function activesync_upgrade19_1()
+{
+	return $GLOBALS['setup_info']['activesync']['currentver'] = '20.1';
+}

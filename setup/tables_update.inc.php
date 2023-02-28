f78ab4d18ca2c5a85c5ee206cbaaf3c0511a1236
ralf <rb@egroupware.org>
2023-02-28 08:49:35 +0100

    bump version to 23.1

--- setup/tables_update.inc.php
+++ setup/tables_update.inc.php
@@ -120,3 +120,13 @@ function activesync_upgrade20_1()
 {
 	return $GLOBALS['setup_info']['activesync']['currentver'] = '21.1';
 }
+
+/**
+ * Bump version to 23.1
+ *
+ * @return string
+ */
+function activesync_upgrade21_1()
+{
+	return $GLOBALS['setup_info']['activesync']['currentver'] = '23.1';
+}
